f6224893d1c184d0a88ba006676f0421b880378d
Hector Labanca <hector.labanca@stormancer.com>
2018-01-04 14:57:57 +0100

    Initial commit

---
+++ Stormancer.Plugins.Management.Server/ManagementPlugin.cs
@@ -0,0 +1,43 @@
+﻿using System.Threading.Tasks;
+using Stormancer.Plugins;
+using Stormancer.Server.Components;
+
+namespace Stormancer.Server.Management
+{
+    public class Startup
+    {
+        public void Run(IAppBuilder builder)
+        {
+            builder.AddPlugin(new ManagementPlugin());
+        }
+    }
+    class ManagementPlugin : IHostPlugin
+    {
+        public void Build(HostPluginBuildContext ctx)
+        {
+            ctx.HostDependenciesRegistration += (IDependencyBuilder b) =>
+              {
+                  b.Register<ManagementClientAccessor>();
+              };
+           
+        }
+
+    }
+
+    public class ManagementClientAccessor
+    {
+        private readonly IEnvironment _environment;
+        public ManagementClientAccessor(IEnvironment environment)
+        {
+            _environment = environment;
+        }
+        public async Task<Stormancer.Management.Client.ApplicationClient> GetApplicationClient()
+        {
+            var infos = await _environment.GetApplicationInfos();
+
+            var result = Stormancer.Management.Client.ApplicationClient.ForApi(infos.AccountId, infos.ApplicationName, infos.PrimaryKey);
+            result.Endpoint = infos.ApiEndpoint;
+            return result;
+        }
+    }
+}
